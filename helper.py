2565778ee52c8e69ca193c99ae35e6c97ee42232
writeriks <emirhaktan@gmail.com>
2024-04-27 23:28:59 +0200

    implement post endpoint to create url

--- helper.py
+++ helper.py
@@ -1,12 +1,12 @@
+import re
+
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 
 
 def return_response(response):
     httpResponse = JSONResponse(content=response)
-    httpResponse.set_cookie(key="emir", value="i am  the one")
     return httpResponse
-    # return Response(content=json.dumps(response, indent=4), media_type="application/json")
 
 
 def return_exception(status_code, detail):
@@ -15,3 +15,25 @@ def return_exception(status_code, detail):
 
 def convert_array_to_dict(array):
     return [item.dict() for item in array]  # Convert each item to dictionary
+
+
+def is_url(input_str):
+    # Regular expression pattern to match URLs
+    url_pattern = re.compile(
+        r"^(?:http|ftp)s?://"  # http:// or https:// or ftp://
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,63}|"  # domain...
+        r"localhost|"  # localhost...
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or IPv4
+        r"(?::\d+)?"  # optional port
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
+
+    return bool(re.match(url_pattern, input_str))
+
+
+def get_object_by_id(id, array):
+    for obj in array:
+        if getattr(obj, "id") == id:
+            return obj
+    return None
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI, Query
 from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
 
-from helper import return_exception
+from helper import get_object_by_id, is_url, return_exception, return_response
 
 app = FastAPI()
 
+urlObjects = []
+
+
+class Url(BaseModel):
+    id: int = None
+    url: str = None
+
+
+class UrlInput(BaseModel):
+    url: str
+
 
 # this provides path parameter validation request should be like /id/1
-@app.get("/id/{id}")
+@app.get("/urls/id/{id}")
 def get_and_redirect(id: int):
-    if id == 1:
-        return RedirectResponse(url="https://www.emiroztrk.com/")
+    # IMPLEMENT FIREBASE OBJECT FETCHING
+    object = get_object_by_id(id, urlObjects)
+    if object:
+        return RedirectResponse(url=getattr(object, "url"))
     else:
         return_exception(404, f"Item with id {id} is not found")
 
@@ -18,7 +32,24 @@ def get_and_redirect(id: int):
 # this provides query parameter validation request should be like /urls/?id=1
 @app.get("/urls/")
 def get_and_redirect(id: int = Query(..., title="ID of the URL")):
-    if id == 1:
-        return RedirectResponse(url="https://www.emiroztrk.com/")
+    # IMPLEMENT FIREBASE OBJECT FETCHING
+    object = get_object_by_id(id, urlObjects)
+    if object:
+        return RedirectResponse(url=getattr(object, "url"))
     else:
         return_exception(404, f"Item with id {id} is not found")
+
+
+@app.post("/url/create")
+def create_url(url_body: UrlInput):
+    url = url_body.url
+
+    if not is_url(url):
+        return_exception(400, "URL is not valid")
+
+    # IMPLEMENT FIREBASE OBJECT CREATION
+    new_url = Url(id=len(urlObjects) + 1, url=url)
+    urlObjects.append(new_url)
+
+    json_data = new_url.dict()
+    return return_response(json_data)
