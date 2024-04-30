b63efdc8ad87607633970059763a8b99cf80dc63
writeriks <emirhaktan@gmail.com>
2024-04-30 23:16:09 +0200

    Implement getting and creating url

--- helper.py
+++ helper.py
@@ -1,23 +1,23 @@
 import re
+from typing import Union
 
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 
 
-def return_response(response):
-    httpResponse = JSONResponse(content=response)
-    return httpResponse
+def return_response(response) -> JSONResponse:
+    return JSONResponse(content=response)
 
 
-def return_exception(status_code, detail):
+def return_exception(status_code, detail) -> HTTPException:
     raise HTTPException(status_code=status_code, detail=detail)
 
 
-def convert_array_to_dict(array):
+def convert_array_to_dict(array) -> list:
     return [item.dict() for item in array]  # Convert each item to dictionary
 
 
-def is_url(input_str):
+def is_url(input_str) -> bool:
     # Regular expression pattern to match URLs
     url_pattern = re.compile(
         r"^(?:http|ftp)s?://"  # http:// or https:// or ftp://
@@ -28,12 +28,11 @@ def is_url(input_str):
         r"(?:/?|[/?]\S+)$",
         re.IGNORECASE,
     )
-
     return bool(re.match(url_pattern, input_str))
 
 
-def get_object_by_id(id, array):
+def get_object_by_id(id, array) -> Union[object, bool]:
     for obj in array:
         if getattr(obj, "id") == id:
             return obj
-    return None
+    return False
--- main.py
+++ main.py
@@ -1,55 +1,36 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
-from helper import get_object_by_id, is_url, return_exception, return_response
+from firebase import create_firebase_document, read_firebase_document
+from helper import is_url, return_exception, return_response
 
 app = FastAPI()
 
-urlObjects = []
 
-
-class Url(BaseModel):
-    id: int = None
+class UrlModel(BaseModel):
     url: str = None
 
 
-class UrlInput(BaseModel):
-    url: str
-
-
 # this provides path parameter validation request should be like /id/1
-@app.get("/urls/id/{id}")
-def get_and_redirect(id: int):
-    # IMPLEMENT FIREBASE OBJECT FETCHING
-    object = get_object_by_id(id, urlObjects)
+@app.get("/urls/{id}")
+def get_and_redirect(id: str):
+    object: UrlModel = read_firebase_document("urls", id)
     if object:
-        return RedirectResponse(url=getattr(object, "url"))
-    else:
-        return_exception(404, f"Item with id {id} is not found")
+        return RedirectResponse(url=object["url"])
+
+    return_exception(404, f"Item with id {id} is not found")
 
 
-# this provides query parameter validation request should be like /urls/?id=1
-@app.get("/urls/")
-def get_and_redirect(id: int = Query(..., title="ID of the URL")):
-    # IMPLEMENT FIREBASE OBJECT FETCHING
-    object = get_object_by_id(id, urlObjects)
-    if object:
-        return RedirectResponse(url=getattr(object, "url"))
-    else:
-        return_exception(404, f"Item with id {id} is not found")
-
-
-@app.post("/url/create")
-def create_url(url_body: UrlInput):
+@app.post("/urls/create")
+def create_url(url_body: UrlModel, request: Request):
     url = url_body.url
 
     if not is_url(url):
         return_exception(400, "URL is not valid")
 
-    # IMPLEMENT FIREBASE OBJECT CREATION
-    new_url = Url(id=len(urlObjects) + 1, url=url)
-    urlObjects.append(new_url)
+    document_data = {"url": url}
+    doc_id = create_firebase_document("urls", document_data)
 
-    json_data = new_url.dict()
-    return return_response(json_data)
+    shortened_url = f"{request.base_url}urls/{doc_id}"
+    return return_response(shortened_url)
